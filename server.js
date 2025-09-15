df6ee1c5cf50f3ca9e6722d5796ab74aac2447e4
jakirazizkhan <jakiridea@gmail.com>
2025-09-16 01:34:34 +0530

    fix CORS error

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.post("/book-appointment", async (req, res) => {
     const { name, age , number , doctor } = req.body;
@@ -40,5 +41,5 @@ app.post("/book-appointment", async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app;
